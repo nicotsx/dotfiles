80740dc859cb7b506383b2958f90bdb072fdbfb7
Nicolas Meienberger <github@thisprops.com>
2024-10-20 14:01:18 +0200

    chore: add nix config

---
+++ .config/nix/README.md
@@ -0,0 +1,34 @@
+# Nix Darwin configuration
+
+## Installation
+
+## Cheatsheet
+
+### Search for a package
+
+#### Nixpkgs
+
+```bash
+nix search nixpkgs <package>
+```
+
+#### Homebrew Mac App Store
+
+```bash
+mas search <package>
+```
+
+This command will search for a package in the Mac App Store and return the ID of the package.
+It can then be added in the `masApps` section of the `flake.nix` file.
+
+```nix
+masApps = {
+  "Tailscale" = "1475387142";
+};
+```
+
+### Apply configuration
+
+```bash
+darwin-rebuild switch --flake ~/.config/nix#developer
+```
---
+++ .config/nix/flake.nix
@@ -0,0 +1,111 @@
+{
+  description = "Blazing fast Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+    let
+      configuration = { pkgs, config, ... }: {
+
+        nixpkgs.config.allowUnfree = true;
+
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages =
+          [
+            pkgs.neovim
+            pkgs.mkalias
+            pkgs.tmux
+            pkgs.warp-terminal
+            pkgs.obsidian
+          ];
+
+        homebrew = {
+          enable = true;
+          brews = [
+            "mas"
+            "stow"
+          ];
+          casks = [
+            "the-unarchiver"
+          ];
+          masApps = {
+            # "Tailscale" = "1475387142";
+          };
+          onActivation.cleanup = "zap";
+        };
+
+        fonts.packages = [
+          (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+        ];
+
+        system.activationScripts.applications.text =
+          let
+            env = pkgs.buildEnv {
+              name = "system-applications";
+              paths = config.environment.systemPackages;
+              pathsToLink = "/Applications";
+            };
+          in
+          pkgs.lib.mkForce ''
+            # Set up applications.
+            echo "setting up /Applications..." >&2
+            rm -rf /Applications/Nix\ Apps
+            mkdir -p /Applications/Nix\ Apps
+            find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+            while read src; do
+              app_name=$(basename "$src")
+              echo "copying $src" >&2
+              ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+            done
+          '';
+
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+        # nix.package = pkgs.nix;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true; # default shell on catalina
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."developer" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "nicolas";
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."developer".pkgs;
+    };
+}
