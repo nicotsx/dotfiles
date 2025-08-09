2535c04e5a25bc93b18d46517f0f50e36df71ffc
Nicolas Meienberger <github@thisprops.com>
2025-08-09 15:02:02 +0200

    try fix

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ config, pkgs, lib, homeDirectory ? "/Users/nicolas", username ? "nicolas", isLinux ? false, ... }:
 
 {
   imports = [
-    ./user.nix
-    ./git.nix
-    ./programs.nix
+    (import ./user.nix { inherit config pkgs lib homeDirectory username isLinux; })
+    (import ./git.nix { inherit config pkgs lib homeDirectory username isLinux; })
+    (import ./programs.nix { inherit config pkgs lib homeDirectory username isLinux; })
   ];
 
   programs.home-manager.enable = true;
