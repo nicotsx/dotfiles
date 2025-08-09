e2913ae2e08ca4d8a584cbc798985158e95018da
Nicolas Meienberger <github@thisprops.com>
2025-08-09 10:27:35 +0200

    refactor(karabiner): remap ducky keyboard cmd keys

--- nix/home/programs.nix
+++ nix/home/programs.nix
@@ -28,7 +28,7 @@
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-        source $HOME/.local/bin/env
+        # source $HOME/.local/bin/env
       '';
 
       shellAliases = {
