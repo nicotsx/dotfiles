a151657bb78219c0571136e2a6e63ee39e253512
Nicolas Meienberger <github@thisprops.com>
2025-08-13 11:17:35 +0200

    feat: add docker completions script

--- nix/home/programs.nix
+++ nix/home/programs.nix
@@ -28,6 +28,8 @@
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+        source ~/dotfiles/zsh/docker-completions.sh
+
         # source $HOME/.local/bin/env
       '';
 
